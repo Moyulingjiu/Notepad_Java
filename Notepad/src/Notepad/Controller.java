22e933bf71b676fc07b541e8157de5dbfa9c1653
BeforeDawn <1597867839@qq.com>
2021-05-08 10:26:42 +0800

    新建项目

---
+++ Notepad/src/Notepad/Controller.java
@@ -0,0 +1,18 @@
+package Notepad;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+
+public class Controller implements Initializable {
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO (don't really need to do anything here).
+	}
+}
---
+++ Notepad/src/Notepad/Main.java
@@ -0,0 +1,28 @@
+package Notepad;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {	
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("Scene.fxml"));
+			
+			primaryStage.setScene(new Scene(root));
+			primaryStage.setTitle("记事本");
+			primaryStage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		launch(args);
+	}
+
+}
