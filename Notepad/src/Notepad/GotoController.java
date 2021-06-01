5b03c30c7c1bf5c14f5455844bbdac2a0d80aafa
BeforeDawn <1597867839@qq.com>
2021-06-01 21:37:26 +0800

    添加转到对话框

---
+++ Notepad/src/Notepad/GotoController.java
@@ -0,0 +1,21 @@
+package Notepad;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class GotoController implements Initializable {
+	private int lineNumber = 1;
+	
+	public int getLine() {
+		return lineNumber;
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO 自动生成的方法存根
+		
+	}
+
+}
